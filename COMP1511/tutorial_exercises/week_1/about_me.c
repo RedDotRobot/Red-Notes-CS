0d851c7b239b9728b6e03f38e6000b6407f09ee1
RedDotRobot <cia.head.of.securities@gmail.com>
2026-02-23 12:53:49 +1100

    COMP1511 Lecture 3

---
+++ COMP1511/tutorial_exercises/week_1/about_me.c
@@ -0,0 +1,18 @@
+// About Me
+// about_me.c
+//
+// This program was written by YOUR-NAME-HERE (zXXXXXXX)
+// on INSERT-DATE-HERE
+//
+// A program to tell us about yourself!
+
+#include <stdio.h>
+
+int main(void) {
+    printf("My favourite food is: Fried rice \n");
+    printf("I'm doing COMP1511 because: I enjoy programming \n");
+    printf("I hope to have achieved: Internship by the time I finish COMP1511.\n");
+    printf("Something I'm passionate about is: Playing instrument \n");
+    printf("My question about COMP1511 that hasn't been answered is: Will I ever need to use Windows in this course \n");
+    return 0;
+}
